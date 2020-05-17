b4e12550c658e1a2ef54affc6af78a7ae12a81b5
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-05-18 01:10:58 +0300

    create MainLayout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+
+  #app
+    component(:is="layout")
+      router-view
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import MainLayout from "./layouts/MainLayout.vue"
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    computed: {
+      layout() {
+        return this.$route.meta.layout
+      }
+    },
+    components: {
+      MainLayout
     }
   }
-}
+
+</script>
+
+<style lang="sass">
+
+  @import "~materialize-css/dist/css/materialize.min.css"
+
+  #app
+    font-family: Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+
 </style>
