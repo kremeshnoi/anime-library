26e39450ef82a89567abce81629294671042648a
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-05-18 01:10:58 +0300

    create MainLayout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+
+div
+  nav.nav-main.blue.darken-1
+    .nav-wrapper
+      a(href="").logo AnimeList
+      a(href="" data-target="nav-mobile").sidenav-trigger.sidenav-trigger-right
+        i.material-icons menu
+
+  ul.sidenav#mobile-demo
+
+  main
+    router-view
+
+</template>
+
+<script>
+
+</script>
+
+<style lang="sass">
+
+.nav
+  &-main
+    display: flex
+    justify-content: center
+    position: relative
+  &-wrapper
+    width: 100%
+
+.sidenav-trigger-right
+  float: right !important
+
+.logo
+  font-size: 1.7rem
+  position: fixed
+  left: 0
+  margin: 0 18px
+
+
+</style>
