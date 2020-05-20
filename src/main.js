b7e40503558b7a863e37a428e7e0e691fa9572c2
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-05-20 15:42:24 +0300

    setup vue meta

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueMeta from 'vue-meta'
 import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
+Vue.use(VueMeta)
 
 new Vue({
   router,
