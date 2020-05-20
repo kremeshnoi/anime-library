7102fa10fb4a30e88872fef1e9c12c70ab498cee
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-05-20 14:25:37 +0300

    move SideNav to separate component

---
+++ src/components/SideNav.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+
+  ul#slide-out.sidenav.sidenav-fixed.sidenav-under
+    li
+      .user-view.user-top.blue.darken-1
+        .background
+          img
+        a(href="")
+          img.circle(src="@/assets/user.jpg")
+    li
+      span.name User
+    li
+      .divider
+    li
+      a.waves-effect.link-position(href="") Home
+    li
+      a.waves-effect.link-position(href="") Search
+    li
+      a.waves-effect.link-position(href="") My Library
+        i.material-icons.icons-position view_module
+    li
+      .divider
+    li
+      .footer-copyright.subheader
+        .container © 2020 Anime Library
+
+</template>
+
+<script>
+
+  export default {
+    name: "SideNav"
+  }
+
+</script>
+
+<style lang="sass" scoped>
+
+  .icons-position
+    margin: 0 0 0 6px !important
+    float: initial !important
+
+  .link-position
+    display: flex !important
+    justify-content: center !important
+
+  .sidenav-under
+    margin-top: 64px !important
+
+  .user-top
+    padding: 32px !important
+
+</style>
