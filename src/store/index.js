d35b623d67a64fcd8ef8e2447bee416ca4dfa056
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-05-26 00:33:05 +0300

    create search Anime service

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import {ApiService} from "../services/search";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    anime: []
+  },
+  mutations: {
+    loadAnime(state, anime) {
+      state.anime = anime
+    }
+  },
+  actions: {
+    async fetchAnime(ctx, query) {
+      const anime = await ApiService.getAnime(query);
+      ctx.commit('loadAnime', anime)
+    }
+  },
+  modules: {},
+  getters: {
+    getAnime: state => state.anime
+  }
 });
