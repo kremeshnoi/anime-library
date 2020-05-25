59cda4160946d1580faa51132ee7c03dfd5c6638
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-05-25 21:08:16 +0300

    change search input border-radius, bg-color

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,20 +2,50 @@
 
 .Search
   h1 Search
+  nav.nav-search
+    .nav-wrapper.white
+      form
+        .input-field
+          input#search(type="search")
+          label.label-icon(for="search")
+            i.material-icons.black-text search
+          i.material-icons close
 
 </template>
 
 <script>
 
+import {searchAnime} from "../services/search"
+
 export default {
   name: "Search",
   metaInfo: {
     title: "Anime Library - Search"
+  },
+  mounted() {
+    searchAnime('naruto')
   }
 }
 
 </script>
 
 <style lang="sass" scoped>
+
+.Search
+  width: 80%
+
+  .nav-search
+    margin-top: 100px
+    border-radius: 3px !important
+
+    .nav-wrapper
+      border-radius: 3px !important
+
+    .input-field
+      border-radius: 3px !important
+      background-color: #ececec !important
+
+      input
+        border-radius: 3px !important
 
 </style>
