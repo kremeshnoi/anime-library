98ab44ff7db4de056eef06ab90fefaddbbffe06b
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-06-05 13:03:51 +0300

    change naming convention

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import {ApiService} from "../services/search";
+import {Search} from "../services/search";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    anime: []
+    animeSearchResult: [],
+    animeTopResult: []
   },
   mutations: {
-    loadAnime(state, anime) {
-      state.anime = anime
+    SET_ANIME(state, animeSearchResult) {
+      state.animeSearchResult = animeSearchResult
     }
   },
   actions: {
-    async fetchAnime(ctx, query) {
-      const anime = await ApiService.getAnime(query);
-      ctx.commit('loadAnime', anime)
+    async getAnime(ctx, query) {
+      const animeSearchResult = await Search.fetchAnime(query);
+      ctx.commit('SET_ANIME', animeSearchResult)
     }
   },
   modules: {},
   getters: {
-    getAnime: state => state.anime
+    fetchAnime: state => state.animeSearchResult
   }
 });
