1402336c580c1299c44a52ad07a776a399d43824
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-06-05 13:04:59 +0300

    update card styles

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -10,19 +10,28 @@
               i.material-icons.black-text.nav-search-icon search
 
     section
-      div.query-content(v-if="getAnime.length")
-        .card(v-for="result in getAnime")
-          .card-image.waves-effect.waves-block.waves-light
-            img.activator(v-bind:src="result")
-          .card-content
-            span.card-title.activator.grey-text.text-darken-4 Card Title
-              i.material-icons.right more_vert
-            p
-              a(href='#') This is a link
-          .card-reveal
-            span.card-title.grey-text.text-darken-4 Card Title
-              i.material-icons.right close
-            p Here is some more information about this product that is only revealed once clicked on.
+      div.query-content(v-if="fetchAnime.length")
+        .card.query-content-card(v-for="result in fetchAnime")
+          .card-image.query-content-card-wrapper
+            img.activator.query-content-card-image(v-bind:src="result.image_url")
+
+          .card-content.query-content-card-outer
+            span.card-title.activator.query-content-card-title {{result.title}}
+
+          .card-reveal.query-content-card-inner
+            span.card-title.query-content-card-inner-title.black-text {{result.title}}
+            p.black-text.query-content-card-inner-text Type : {{result.type}}
+            p.black-text.query-content-card-inner-text Score : {{result.score}}
+            .input-field
+              select.browser-default.query-content-card-inner-select
+                option(value='') Status
+                option(value='1') Plan to watch
+                option(value='2') Watching
+                option(value='3') Completed
+                option(value='4') Dropped
+              label
+            p.query-content-card-inner-text
+              a.blue-text(href="") More info
 
 </template>
 
@@ -39,19 +48,23 @@
       title: "Anime Library - Search"
     },
     methods: {
-      ...mapActions(['fetchAnime']),
+      ...mapActions(['getAnime']),
       getResult(query) {
-        this.fetchAnime(query)
+        this.getAnime(query)
       }
     },
     computed: {
-      ...mapGetters(['getAnime'])
+      ...mapGetters(['fetchAnime'])
+    },
+    mounted() {
     }
   }
 
 </script>
 
 <style lang="sass" scoped>
+
+  @import "../assets/styles/utils/index"
 
   .Search
     width: 100%
@@ -62,10 +75,12 @@
     justify-content: flex-start
 
     .nav-search
-      width: 60%
+      width: 70%
       height: 50px
       margin: 100px 0 80px 0
       border-radius: 3px
+      +mq(phone, max)
+        width: 90%
 
       &-wrapper
         border-radius: 3px
@@ -83,8 +98,51 @@
 
     .query-content
       display: grid
-      grid-template-columns: repeat(5, 1fr)
+      grid-template-columns: repeat(auto-fill, 228px)
+      justify-content: center
       grid-gap: 20px
 
+      &-card
+        margin: 0
+
+        &-wrapper
+          height: 84%
+          cursor: pointer
+          padding: 16px 0 0 0
+
+        &-title
+          text-overflow: ellipsis
+          overflow: hidden
+          width: 160px
+          display: inline-block
+          white-space: nowrap
+          margin: 0
+          font-size: 16px
+
+        &-image
+          height: 100%
+          max-height: 280px
+          object-fit: contain
+
+        &-outer
+          display: flex
+          justify-content: center
+          align-items: center
+          padding: 10px
+
+        &-inner
+          &-title
+            font-size: 16px
+
+          &-select
+            border: none
+            padding: 0
+            outline: none
+
+          &-text
+            text-align: start
+
+            &:first-of-type
+              margin: 30px 0 0 0
 
 </style>
