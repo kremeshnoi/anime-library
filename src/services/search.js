0bd86ebb0368d66e7eea474d9dfa8e781f37bb9e
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-06-07 23:24:02 +0300

    minor changes, change to double quotes

--- src/services/search.js
+++ src/services/search.js
@@ -3,7 +3,7 @@ import jikanjs from "../../node_modules/jikanjs/lib/jikan.js"
 const fetchAnime = async (query) => {
   try {
     const data = [];
-    const response = await jikanjs.search('anime', query);
+    const response = await jikanjs.search("anime", query);
     console.log(response.results)
     response.results.forEach(element => {
       data.push(element)
