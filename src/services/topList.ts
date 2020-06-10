1b0fe00a4ee02271eb3d7f5f3ec3a5acc1ced2f0
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-06-10 21:07:47 +0300

    add try catch

--- src/services/topList.ts
+++ src/services/topList.ts
@@ -1,13 +1,17 @@
 import jikanjs from "../../node_modules/jikanjs/lib/jikan.js"
 
-const fetchTopList = async (page: number, subtype: string) => {
-	const data: Array<object> = [];
-	const response = await jikanjs.loadTop("anime", page, subtype);
-	console.log(response.results);
-	response.top.forEach((element: any) => {
-		data.push(element)
-	 })
-	return data;
+const fetchTopList = async (type: string, page: number, subtype: string) => {
+	try {
+		const data: Array<object> = [];
+		const response = await jikanjs.loadTop(type, page, subtype);
+		response.top.forEach((element: any) => {
+			data.push(element)
+		})
+		return data;
+	}  catch (error) {
+		throw new Error(error)
+	}
+
  }
 
 const TopList = {
--- src/store/index.ts
+++ src/store/index.ts
@@ -13,39 +13,43 @@ Vue.use(Vuex);
 export const state: State = {
 	animeSearchResult: [],
 	animeTopListResult: []
- }
+}
 
 export default new Vuex.Store({
 	state,
 	mutations: {
 		SET_ANIME(state, animeSearchResult) {
 			state.animeSearchResult = animeSearchResult
-		 },
+		},
 		SET_ANIME_TOP(state, animeTopListResult) {
 			state.animeTopListResult = animeTopListResult
-		 }
-	 },
+		}
+	},
 	actions: {
 		async getAnime(ctx, query) {
 			try {
 				const animeSearchResult = await Search.fetchAnime(query);
 				if (Array.isArray(animeSearchResult) && animeSearchResult.length > 0) {
 					ctx.commit("SET_ANIME", animeSearchResult)
-				 } else {
+				} else {
 					MaterialToastService.toast("Anime not found")
-				 }
-			 } catch (e) {
+				}
+			} catch (e) {
 				console.log(e)
-			 }
-		 },
-		async getTopList(ctx, { page, subtype }) {
-			const animeTopListResult = await TopList.fetchTopList(page, subtype);
-			ctx.commit("SET_ANIME_TOP", animeTopListResult)
-		 }
-	 },
+			}
+		},
+		async getTopList(ctx, { type, page, subtype }) {
+			try {
+				const animeTopListResult = await TopList.fetchTopList(type, page, subtype);
+				ctx.commit("SET_ANIME_TOP", animeTopListResult)
+			} catch (e) {
+				console.log(e)
+			}
+		}
+	},
 	modules: {  },
 	getters: {
 		fetchAnime: state => state.animeSearchResult,
 		fetchTopList: state => state.animeTopListResult
-	 }
- });
+	}
+});
