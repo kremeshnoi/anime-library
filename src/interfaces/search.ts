b99c59e6cfab3c43c99016cf6de845074cdc90d8
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-06-10 18:31:41 +0300

    create interfaces

---
+++ src/interfaces/search.ts
@@ -0,0 +1,25 @@
+interface SearchResponse {
+	"request_hash": string;
+	"request_cached": boolean;
+	"request_cache_expiry": number;
+	"last_page": number;
+	results: SearchResult[];
+ }
+
+interface SearchResult {
+	"mal_id": number;
+	"url": string;
+	"image_url": string;
+	"title": string;
+	"type": string;
+	"episodes"?: number;
+	"airing": boolean;
+	"rating": string;
+	"score": number;
+	"members": number;
+	"synopsis": string;
+	"end_date"?: string;
+	"rated": string;
+ }
+
+export { SearchResponse, SearchResult }
