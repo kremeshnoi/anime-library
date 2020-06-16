0a70b6b0448133d741436d0bafe49c030e420fdc
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-06-16 17:02:09 +0300

    create SignUp

---
+++ src/views/SignUp.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+
+	.SignUp
+		p.SignUp-title Start using AnimeLibrary
+		form
+			.SignUp-row
+				.input-field
+					input#email.validate(type="email" name="email")
+					label(for='email') Email
+
+			.SignUp-row
+				.input-field
+					input#username.validate(type="text" name="username")
+					label(for='username') Username
+
+			.SignUp-row
+				.input-field
+					input#password.validate(type="password" name="password")
+					label(for='password') Password
+
+			.SignUp-row
+				router-link(to="/sign-in") Already have an account?
+
+			center
+				.SignUp-row
+					vue-recaptcha(sitekey="6Lc-DaUZAAAAABeSVHxIZhS9Wk2xqSo53V4UeX-H")
+
+				.SignUp-row
+							button.SignUp-button.btn-large(type="submit" name="submit_registration") Create Account
+
+		.SignUp-cancel
+			router-link(to="/") Back to Homepage?
+
+</template>
+
+<script lang="ts">
+
+	import VueRecaptcha from 'vue-recaptcha';
+
+	export default {
+		name: "SignUp",
+		components: {
+			VueRecaptcha
+		},
+		metaInfo: {
+			title: "Anime Library - Sign Up"
+		}
+	}
+
+</script>
+
+<style lang="sass" scoped>
+
+	@import "../assets/styles/utils/vars"
+	@import "../assets/styles/utils/mixins"
+	@import "../assets/styles/modules/buttons"
+
+	.SignUp
+		position: fixed
+		top: 0
+		left: 0
+		right: 0
+		bottom: 0
+		margin: auto
+		max-width: 400px
+		max-height: 570px
+		border-radius: 4px
+		padding: 20px 20px 20px 20px
+		background-color: $color-pure-white
+		box-shadow: 10px 10px 5px 0 rgba(0, 0, 0, 0.75)
+
+		&-title
+			padding: 20px 0 0 0
+			font-size: 22px
+
+		&-row
+			margin: 20px auto 20px auto
+			max-width: 290px
+
+		&-button
+			@extend .hover-btn
+			@extend .shadow-btn
+			@extend .focus-btn
+
+</style>
