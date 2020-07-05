7fa8953e7b819b8d519a93f529d59c3e94eba397
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-07-05 09:39:15 +0300

    remove unnecessary variables

--- src/services/fetchAnime.ts
+++ src/services/fetchAnime.ts
@@ -1,15 +1,11 @@
 import jikanjs from "../../node_modules/jikanjs/lib/jikan.js";
 
-import { SearchResponse, SearchResult } from "@/interfaces/search.ts";
+import { SearchResponse } from "@/interfaces/search.ts";
 
 const fetchAnime = async (query: string) => {
   try {
-    const data: SearchResult[] = [];
     const response: SearchResponse = await jikanjs.search("anime", query);
-    response.results.forEach(element => {
-      data.push(element);
-    });
-    return data;
+    return response.results
   } catch (error) {
     throw new Error(error);
   }
--- src/services/fetchTopList.ts
+++ src/services/fetchTopList.ts
@@ -2,12 +2,8 @@ import jikanjs from "../../node_modules/jikanjs/lib/jikan.js";
 
 const fetchTopList = async (type: string, page: number, subtype?: string) => {
   try {
-    const data: Array<object> = [];
     const response = await jikanjs.loadTop(type, page, subtype);
-    response.top.forEach((element: any) => {
-      return data.push(element);
-    });
-    return data;
+    return response.top
   } catch (error) {
     throw new Error(error);
   }
