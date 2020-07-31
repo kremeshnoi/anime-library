9df9fbaf1c8cc187ec568b2613f904a076965b5a
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-07-31 04:21:12 +0300

    create SearchBar module component

---
+++ src/components/modules/SearchBar.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+
+	.search-bar
+		nav.search-bar__nav
+			.search-bar__wrapper.white.nav-wrapper
+				form.search-bar__form(v-on:submit.prevent="loadAnimeSearched(query)")
+					.search-bar__field.input-field
+						input#search.search-bar__input(type="search", v-model="query")
+						label.search-bar__label.label-icon(for="search")
+							i.search-bar__icon.material-icons.black-text search
+
+</template>
+
+<script>
+
+	import { mapActions, mapGetters } from "vuex";
+
+	export default {
+		name: "SearchBar",
+		data: () => ({
+			query: ""
+		}),
+		methods: {
+			...mapActions(["loadAnimeSearched"])
+		}
+	};
+
+</script>
+
+<style lang="sass" scoped>
+
+	@import "../../assets/styles/utils/vars.sass"
+	@import "../../assets/styles/utils/mixins.sass"
+
+	.search-bar
+		width: 100%
+
+		&__nav
+			width: 100%
+			height: 50px
+			margin: 0 0 54px
+			border-radius: 3px
+
+		&__wrapper
+			border-radius: 3px
+
+		&__field
+			border-radius: 3px
+			background-color: $color-white
+
+		&__input
+			border-radius: 3px
+
+		&__icon
+			height: 50px
+			line-height: 50px
+
+</style>
