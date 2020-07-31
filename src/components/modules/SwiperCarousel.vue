7a33ebe09f8491601586072dcaa767172c7c6a4b
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-08-01 02:45:07 +0300

    refactor Cards component to SwiperCarousel

--- src/components/modules/SwiperCarousel.vue
+++ src/components/modules/SwiperCarousel.vue
@@ -1,12 +1,8 @@
 <template lang="pug">
 
-	.Cards(v-swiper:mySwiper="swiperOptions")
-		.Cards-container.swiper-wrapper
-			.Cards-item.card.swiper-slide(v-for="(result, index) in getAnimeAiring" :key="index")
-				.Cards-image-wrapper.card-image
-					img.Cards-image.activator(@click="computeRoute(result)" :src="result['image_url']")
-				.Cards-title-wrapper.card-content
-					span.Cards-title.card-title.activator {{ result['title'] }}
+	.swiper-carousel(v-swiper:mySwiper="swiperOptions")
+		.swiper-carousel__container.swiper-wrapper
+			Card.swiper-slide(:query="result" v-for="(result, index) in getAnimeAiring" :key="index")
 
 		.swiper-button-prev
 		.swiper-button-next
@@ -17,6 +13,7 @@
 
 	import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
 	import { mapActions, mapGetters } from "vuex";
+	import Card from "@/components/modules/Card";
 
 	export default {
 		name: "Cards",
@@ -58,6 +55,7 @@
 			};
 		},
 		components: {
+			Card,
 			Swiper,
 			SwiperSlide
 		},
@@ -82,67 +80,8 @@
 	@import "../../assets/styles/utils/vars.sass"
 	@import "../../assets/styles/utils/mixins.sass"
 
-	.Cards
+	.swiper-carousel
 		width: 100%
-
-		&-item
-			margin: 10px 10px 10px 0
-			height: fit-content
-			box-shadow: none
-
-			&:hover
-				opacity: 0.9
-
-		&-image
-			object-fit: cover
-			transform: scale(1.02)
-
-			&-wrapper
-				overflow: hidden
-				cursor: pointer
-
-		&-title
-			margin: 0 !important
-			width: 160px
-			font-size: 16px
-			display: block
-			overflow: hidden
-			white-space: nowrap
-			text-overflow: ellipsis
-
-			&-wrapper
-				display: flex
-				justify-content: center
-				align-items: center
-				padding: 10px
-				box-shadow: 0 8px 7px -7px rgba(0, 0, 0, 1)
-
-			&-title
-				margin: 0 auto
-				font-size: 16px
-				max-width: 150px
-
-			&-description
-				text-align: start
-				margin: 8px 0 8px 0
-
-				&:first-of-type
-					margin: 24px 0 0 0
-
-			&-input-field
-				+flex(center, center, column)
-
-			&-label
-				width: 100%
-				text-align: start
-				position: initial
-				margin: 0 0 8px 0
-
-			&-select
-				margin: 0
-				padding: 0
-				border: none
-				outline: none
 
 	.swiper-button-prev, .swiper-button-next
 		top: 0
@@ -160,7 +99,6 @@
 		background: linear-gradient(to left, rgba(213, 213, 213, 0), rgba(213, 213, 213, 0.75) 70%, rgba(213, 213, 213, 0.85))
 		border-bottom-right-radius: 100% 50%
 		border-top-right-radius: 100% 50%
-
 
 	.swiper-button-next
 		right: 0
