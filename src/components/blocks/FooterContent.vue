01c7a9381283dd75cb638e5fcb79badd3801220b
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-08-07 03:30:53 +0300

    refactor FooterContent to BEM

--- src/components/blocks/FooterContent.vue
+++ src/components/blocks/FooterContent.vue
@@ -1,34 +1,34 @@
 <template lang="pug">
 
-	.FooterContent.page-footer
-		.FooterContent-container
-			.FooterContent-item
-				h5.FooterContent-title
+	.footer-content.page-footer
+		.footer-content__container
+			.footer-content__item
+				h5.footer-content__title
 					| Discord channel
-				p.FooterContent-description
+				p.footer-content__description
 					| We will also be happy to meet you on our channel, where you can always find like-minded people. Feel free to come in.
-				a.FooterContent-button.btn(href="https://discord.com/invite/Ku72D5/"
-													target="_blank")
+				a.footer-content__link_decor.btn(href="https://discord.com/invite/Ku72D5/"
+														target="_blank")
 					| Chat
 
-			.FooterContent-item
-				h5.FooterContent-title
+			.footer-content__item
+				h5.footer-content__title
 					| Sitemap
-				router-link.FooterContent-link(to="")
+				router-link.footer-content__link(to="")
 					| About project
-				h6.FooterContent-subtitle
+				h6.footer-content__subtitle
 					| Sign in required
-				router-link.FooterContent-link(to="")
+				router-link.footer-content__link(to="")
 					| Search page
-				router-link.FooterContent-link(to="")
+				router-link.footer-content__link(to="")
 					| Your library
 
-			.FooterContent-item
-				h5.FooterContent-title
+			.footer-content__item
+				h5.footer-content__title
 					| Help
-				router-link.FooterContent-link(to="/recovery")
+				router-link.footer-content__link(to="/recovery")
 					| Forgot Password?
-				router-link.FooterContent-link(to="")
+				router-link.footer-content__link(to="")
 					| Q/A
 
 </template>
@@ -47,12 +47,12 @@
 	@import "../../assets/styles/utils/mixins.sass"
 	@import "../../assets/styles/modules/buttons.sass"
 
-	.FooterContent
+	.footer-content
 		text-align: start
 		padding: 0
 		background-color: $color-blue
 
-		&-container
+		&__container
 			width: 100%
 			max-width: 1300px
 			margin: 0 auto
@@ -64,40 +64,37 @@
 			justify-content: space-between
 			grid-template-columns: repeat(3, 1fr)
 
-		&-title
+		&__title
 			padding: 0 0 8px 0
 
-		&-subtitle
+		&__subtitle
 			padding: 16px 0 8px 0
 			border-bottom: 1px solid rgba($color-orange, 0.6)
 
-		&-link
+		&__link
 			margin: 8px 0 0 0
 			color: $color-white
-
 			&:hover
 				opacity: 0.6
+			&_decor
+				margin: 16px 0 0 0
+				@extend .shadow-btn
+				@extend .hover-btn
+				@extend .focus-btn
 
-		&-item
+		&__item
 			height: 100%
 			padding: 0 24px 0 0
 			justify-content: flex-start !important
 			border-right: 1px solid rgba(225, 225, 225, 0.2)
 			+flex(center, flex-start, column)
-
 			&:last-of-type
 				border: none
 
-		&-description
+		&__description
 			margin: 8px 0 0 0
 			max-width: 300px
 			+mq(tablet-mid, max)
 				max-width: 70%
-
-		&-button
-			margin: 16px 0 0 0
-			@extend .shadow-btn
-			@extend .hover-btn
-			@extend .focus-btn
 
 </style>
