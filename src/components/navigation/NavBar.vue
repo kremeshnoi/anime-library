dc4e57d6e08724e440f773b87e6d3a72bbab3b38
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-08-20 08:31:13 +0300

    remove login link from navbar

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -5,11 +5,6 @@
 			router-link.nav-global-logo.nav-global-link( to="/" )
 				| Otaku Library
 
-			.nav-global-logout
-				router-link.nav-global-logout-link( to="/login" )
-					| Sign in
-					i.material-icons.nav-global-logout-icon exit_to_app
-
 </template>
 
 <script>
@@ -27,46 +22,20 @@
 
 	.nav
 		&-global
-			display: flex
-			justify-content: center
-			position: fixed
 			height: 54px
 			line-height: 54px
 			background-color: $color-blue
 			top: 0
+			position: fixed
 			z-index: 99
 			padding: 0 40px 0 40px
 
 			&-wrapper
 				width: 100%
-				+flex(flex-end, initial, row)
 
 			&-logo
 				font-size: 24px
 				color: $color-white
-				position: absolute
-				right: 50%
-				transform: translateX(50%)
 				font-family: "Days One", sans-serif
-
-			&-logout
-				display: flex
-				justify-content: flex-end
-				width: auto
-				z-index: 999
-				+mq(tablet-wide, max)
-					display: none
-				&-icon
-					height: 54px
-					line-height: 54px
-					margin: 0 0 0 6px
-
-				&-link
-					display: flex
-					align-items: center
-
-					&:hover
-						.nav-global-logout-icon
-							transform: translateX(4px)
 
 </style>
