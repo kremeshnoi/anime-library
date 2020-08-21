38d87e2aca3f7e4841b5b7b91c330d4ad929bf9e
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-08-21 20:42:05 +0300

    put query content to a separated component

--- src/components/views/Search.vue
+++ src/components/views/Search.vue
@@ -3,7 +3,7 @@
 	.search
 		.search__container
 			search-bar
-			.search__query-content( v-if="getAnimeSearched.length" )
+			query-content
 				cards( :query="result"
 						 v-for="result in getAnimeSearched" )
 
@@ -13,13 +13,15 @@
 
 	import { mapActions, mapGetters } from "vuex";
 	import SearchBar from "@/components/modules/SearchBar.vue";
+	import QueryContent from "@/components/modules/QueryContent.vue";
 	import Cards from "@/components/modules/Cards.vue";
 
 	export default {
 		name: "Search",
 		components: {
-			Cards,
-			SearchBar
+			SearchBar,
+			QueryContent,
+			Cards
 		},
 		metaInfo: {
 			title: "Otaku Library / Search"
@@ -34,28 +36,11 @@
 <style lang="sass" scoped>
 
 	@import "../../assets/styles/modules/containers.sass"
-	@import "../../assets/styles/utils/vars.sass"
-	@import "../../assets/styles/utils/mixins.sass"
 
 	.search
 		width: 100%
 
 		&__container
 			@extend .container-default
-
-		&__query-content
-			display: grid
-			grid-template-columns: repeat(5, 1fr)
-			justify-content: center
-			grid-gap: 20px
-			height: 100%
-			+mq(desktop, max)
-				grid-template-columns: repeat(4, 1fr)
-			+mq(tablet-mid, max)
-				grid-template-columns: repeat(3, 1fr)
-			+mq(tablet, max)
-				grid-template-columns: repeat(2, 1fr)
-			+mq(phablet-small, max)
-				grid-template-columns: repeat(1, 1fr)
 
 </style>
