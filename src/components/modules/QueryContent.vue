162e8f789bd7ae904a9938ecabdb6c424ec98a2f
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-08-21 20:42:05 +0300

    put query content to a separated component

---
+++ src/components/modules/QueryContent.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+
+	.query-content
+		slot
+
+</template>
+
+<script>
+
+	export default {
+		name: "QueryContent"
+	}
+
+</script>
+
+<style lang="sass" scoped>
+
+	@import "../../assets/styles/utils/vars.sass"
+	@import "../../assets/styles/utils/mixins.sass"
+
+	.query-content
+		display: grid
+		grid-template-columns: repeat(5, 1fr)
+		justify-content: center
+		grid-gap: 20px
+		height: 100%
+		+mq(desktop, max)
+			grid-template-columns: repeat(4, 1fr)
+		+mq(tablet-mid, max)
+			grid-template-columns: repeat(3, 1fr)
+		+mq(tablet, max)
+			grid-template-columns: repeat(2, 1fr)
+		+mq(phablet-small, max)
+			grid-template-columns: repeat(1, 1fr)
+
+</style>
