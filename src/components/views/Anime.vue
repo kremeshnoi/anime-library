1827dbe3c387a95678e344915a20da526336e238
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-08-31 23:11:03 +0300

    update Anime view component

--- src/components/views/Anime.vue
+++ src/components/views/Anime.vue
@@ -61,17 +61,63 @@
 								| {{ getAnimeById["rating"] }}
 
 			.anime__sub-content
+				.anime__trailer.anime-trailer
+					.anime-trailer__title
+						| Watch Trailer
+					.anime-trailer__content
+						iframe.anime-trailer__iframe( v-if="getAnimeById['trailer_url']"
+																:src="getAnimeById['trailer_url']"
+																frameborder="0"
+																allowfullscreen="true"
+																autoplay="false")
+						h2.anime-trailer__info( v-else ) Trailer not found -_-
+
+				.anime__related.anime-related
+					.anime-related__title
+						| Related
+					.anime-related__content
+						ul.anime-related__tabs.tabs( v-if="getAnimeById['related']" )
+							li.anime-related__tab.tab( v-for="(value, name, counter) in getAnimeById['related']"
+															  :key="name" )
+								a.anime-related__tab-item( :href=`'#' + counter` )
+									| {{ name }}
+						.anime-related__item( :id="counter"
+													v-for="(value, name, counter) in getAnimeById['related']"
+													:key="name" )
+							table.anime-related__table.striped
+								tbody.anime-related__tbody
+									tr.anime-related__tr( v-for="(result, index) in value"
+																:key="index" )
+										td.anime-related__td
+											a.anime-related__link( @click="computeRoute(result)")
+												| {{ result["name"] }}
+
+			.anime__description.anime-description
+				.anime-description__title
+					| DESCRIPTION
+
+				.anime-description__synopsis
+					| {{ getAnimeById["synopsis"] }}
 
 </template>
 
 <script>
 
 	import { mapActions, mapGetters } from "vuex";
+	import Cards from "@/components/modules/Cards";
+	import SwiperCarousel from "@/components/modules/SwiperCarousel";
 	import SelectCollection from "@/components/modules/SelectCollection";
 
 	export default {
 		name: "Anime",
+		data() {
+			return {
+				relatedData: []
+			}
+		},
 		components: {
+			Cards,
+			SwiperCarousel,
 			SelectCollection
 		},
 		metaInfo() {
@@ -80,13 +126,18 @@
 			}
 		},
 		methods: {
-			...mapActions(["loadAnimeById"])
+			...mapActions(["loadAnimeById", "computeRoute"])
 		},
 		computed: {
 			...mapGetters(["getAnimeById"])
 		},
 		async created() {
 			await this.loadAnimeById();
+			const tabs = document.querySelectorAll(".tabs");
+			const instanceTabs = M.Tabs.init(tabs);
+			const tooltips = document.querySelectorAll(".tooltipped");
+			const instanceTooltips = M.Tooltip.init(tooltips);
+			const indicatorTooltip = document.querySelector(".indicator").style.display = "none";
 		}
 	};
 
@@ -94,10 +145,15 @@
 
 <style lang="sass" scoped>
 
+	// IMPORTS
+
 	@import "../../assets/styles/utils/vars"
 	@import "../../assets/styles/utils/mixins"
+	@import "../../assets/styles/modules/titles"
 	@import "../../assets/styles/modules/dividers"
 	@import "../../assets/styles/modules/containers"
+
+	// ANIME
 
 	.anime
 		width: 100%
@@ -106,18 +162,35 @@
 			display: grid
 			grid-gap: 20px
 			grid-template-columns: 1fr 1fr
+			grid-template-areas: "main sub" "description description"
 			@extend .container-default
+			+mq(tablet-mid, max)
+				grid-template-columns: 1fr
+				grid-template-areas: "main" "sub" "description"
 
 		&__main-content
+			grid-area: main
 			display: grid
 			justify-content: start
 			grid-template-areas: "title title" "cover info"
 			grid-gap: 20px
-
-		&__input-field
-			margin: 0
+			align-content: start
+			+mq(phablet, max)
+				grid-template-areas: "title" "cover" "info"
 
 		&__sub-content
+			grid-area: sub
+			display: grid
+			grid-gap: 20px
+			justify-content: flex-start
+			grid-template-rows: min-content
+			grid-template-columns: minmax(auto, 360px)
+
+		&__description
+			grid-area: description
+
+		&__info
+			grid-area: info
 
 		&__title
 			text-align: start
@@ -127,10 +200,68 @@
 
 		&__cover
 			grid-area: cover
+			width: 100%
+
+		&__input-field
+			margin: 0
+
+	// ANIME TRAILER
+
+	.anime-trailer
+		display: grid
+		justify-content: start
+		grid-gap: 20px
+
+		&__title
+			@extend .header-title
+
+		&__iframe
+			height: 200px
+			width: 360px
+			+mq(phone-wide, max)
+				height: 124px
+				width: 220px
+
+	// ANIME RELATED
+
+	.anime-related
+		&__title
+			@extend .header-title
+
+		&__tabs
+			display: flex
+			flex-wrap: wrap
+			overflow-x: initial
+			overflow-y: initial
+			height: 100%
+
+		&__tab-item
+			padding: 0 !important
+			margin: 0 24px 0 0
+			text-align: start
+			color: $color-blue-light !important
+			&.active
+				background-color: initial !important
+				color: $color-orange !important
+
+		&__link
+			color: $color-grey-dark
+			&:hover
+				text-decoration: underline
+
+	// ANIME DESCRIPTION
+
+	.anime-description
+		text-align: start
+
+		&__title
+			@extend .header-title
+			margin: 0 0 16px 0
+
+	// ANIME INFO
 
 	.anime-info
 		text-align: start
-		grid-area: info
 		+flex(initial, initial, column)
 
 		&__title
@@ -160,6 +291,7 @@
 
 		&__list-value
 			display: inline-block
+
 			&_decor
 				padding: 0 6px
 				border-radius: 4px
