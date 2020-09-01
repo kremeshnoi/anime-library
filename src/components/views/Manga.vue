62de8e40dac94d20ee0342f7d4520708c5ead5db
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-01 23:39:23 +0300

    update Manga view component

--- src/components/views/Manga.vue
+++ src/components/views/Manga.vue
@@ -44,13 +44,17 @@
 							.manga-info__list-key
 								| Volumes:
 							| &nbsp;
-							.manga-info__list-value
+							.manga-info__list-value( v-if="getMangaById['volumes'] === null" )
+								| Unknown
+							.manga-info__list-value( v-else )
 								| {{ getMangaById["volumes"] }}
 						li.manga-info__list-item
 							.manga-info__list-key
 								| Chapters:
 							| &nbsp;
-							.manga-info__list-value
+							.manga-info__list-value( v-if="getMangaById['chapters'] === null" )
+								| Unknown
+							.manga-info__list-value( v-else )
 								| {{ getMangaById["chapters"] }}
 						li.manga-info__list-item
 							.manga-info__list-key
@@ -61,6 +65,32 @@
 								| {{ result["name"] }}
 
 			.manga__sub-content
+				.manga__related.anime-related
+					.manga-related__title
+						| Related
+					.manga-related__content
+						ul.manga-related__tabs.tabs( v-if="getMangaById['related']" )
+							li.manga-related__tab.tab( v-for="(value, name, counter) in getMangaById['related']"
+								:key="name" )
+								a.manga-related__tab-item( :href=`'#' + counter` )
+									| {{ name }}
+						.manga-related__item( :id="counter"
+							v-for="(value, name, counter) in getMangaById['related']"
+							:key="name" )
+							table.manga-related__table.striped
+								tbody.manga-related__tbody
+									tr.manga-related__tr( v-for="(result, index) in value"
+										:key="index" )
+										td.manga-related__td
+											a.manga-related__link( @click="computeRoute(result)")
+												| {{ result["name"] }}
+
+			.manga__description.manga-description
+				.manga-description__title
+					| DESCRIPTION
+
+				.manga-description__synopsis
+					| {{ getMangaById["synopsis"] }}
 
 </template>
 
@@ -80,13 +110,16 @@
 			}
 		},
 		methods: {
-			...mapActions(["loadMangaById"])
+			...mapActions(["loadMangaById", "computeRoute"])
 		},
 		computed: {
 			...mapGetters(["getMangaById"])
 		},
 		async created() {
 			await this.loadMangaById();
+			const tabs = document.querySelectorAll(".tabs");
+			const instanceTabs = M.Tabs.init(tabs);
+			const indicatorTooltip = document.querySelector(".indicator").style.display = "none";
 		}
 	};
 
@@ -94,10 +127,15 @@
 
 <style lang="sass" scoped>
 
+	// IMPORTS
+
 	@import "../../assets/styles/utils/vars"
 	@import "../../assets/styles/utils/mixins"
+	@import "../../assets/styles/modules/titles"
 	@import "../../assets/styles/modules/dividers"
 	@import "../../assets/styles/modules/containers"
+
+	// MANGA
 
 	.manga
 		width: 100%
@@ -106,18 +144,35 @@
 			display: grid
 			grid-gap: 20px
 			grid-template-columns: 1fr 1fr
+			grid-template-areas: "main sub" "description description"
 			@extend .container-default
+			+mq(tablet-mid, max)
+				grid-template-columns: 1fr
+				grid-template-areas: "main" "sub" "description"
 
 		&__main-content
+			grid-area: main
 			display: grid
 			justify-content: start
 			grid-template-areas: "title title" "cover info"
 			grid-gap: 20px
-
-		&__input-field
-			margin: 0
+			align-content: start
+			+mq(phablet, max)
+				grid-template-areas: "title" "cover" "info"
 
 		&__sub-content
+			grid-area: sub
+			display: grid
+			grid-gap: 20px
+			justify-content: flex-start
+			grid-template-rows: min-content
+			grid-template-columns: minmax(auto, 360px)
+
+		&__description
+			grid-area: description
+
+		&__info
+			grid-area: info
 
 		&__title
 			text-align: start
@@ -128,52 +183,93 @@
 		&__cover
 			grid-area: cover
 
-	.manga-info
-		text-align: start
-		grid-area: info
-		+flex(initial, initial, column)
+		&__input-field
+			margin: 0
 
-		&__title
-			color: $color-black
-			width: 100%
-			font-size: 16px
-			font-weight: 700
+		// MANGA RELATED
+
+		.manga-related
+			&__title
+				@extend .header-title
+
+			&__tabs
+				display: flex
+				flex-wrap: wrap
+				overflow-x: initial
+				overflow-y: initial
+				height: 100%
+
+			&__tab-item
+				padding: 0 !important
+				margin: 0 24px 0 0
+				text-align: start
+				color: $color-blue-light !important
+				&.active
+					background-color: initial !important
+					color: $color-orange !important
+
+			&__link
+				color: $color-grey-dark
+				&:hover
+					text-decoration: underline
+
+		// MANGA DESCRIPTION
+
+		.manga-description
 			text-align: start
-			text-transform: uppercase
-			padding: 14px 0 14px 14px
-			border-left: 5px solid $color-blue-light
 
-		&__list
-			margin: 20px 0 0 5px
-			height: 100%
-			max-width: 300px
-			width: 100%
-			+flex(space-between, initial, column)
+			&__title
+				@extend .header-title
+				margin: 0 0 16px 0
 
-		&__list-item
-			margin: 6px 0
-			&:last-of-type
-				margin: 0
+		// MANGA INFO
 
-		&__list-key
-			display: inline-block
+		.manga-info
+			text-align: start
+			grid-area: info
+			+flex(initial, initial, column)
 
-		&__list-value
-			display: inline-block
-			&_decor
-				padding: 0 6px
-				border-radius: 4px
-				color: $color-white-pure
-				background-color: $color-yellow
+			&__title
+				color: $color-black
+				width: 100%
+				font-size: 16px
+				font-weight: 700
+				text-align: start
+				text-transform: uppercase
+				padding: 14px 0 14px 14px
+				border-left: 5px solid $color-blue-light
 
-		&__list-values
-			margin: 0 10px 0 0
-			height: auto
-			transition: 0.5s
-			display: inline-block
-			border-bottom: 1px dashed $color-grey-light
-			&:hover
-				cursor: pointer
-				border-bottom: 1px dashed $color-blue
+			&__list
+				margin: 20px 0 0 5px
+				height: 100%
+				max-width: 300px
+				width: 100%
+				+flex(space-between, initial, column)
+
+			&__list-item
+				margin: 6px 0
+				&:last-of-type
+					margin: 0
+
+			&__list-key
+				display: inline-block
+
+			&__list-value
+				display: inline-block
+				&_decor
+					padding: 0 6px
+					border-radius: 4px
+					color: $color-white-pure
+					background-color: $color-yellow
+
+			&__list-values
+				margin: 0 10px 0 0
+				height: auto
+				transition: 0.5s
+				display: inline-block
+				border-bottom: 1px dashed $color-grey-light
+				&:hover
+					cursor: pointer
+					border-bottom: 1px dashed $color-blue
 
 </style>
