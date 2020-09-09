2039070bd2d909814d2ef7a834a68ebb1ec772ae
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-09 21:31:42 +0300

    fill the character with content

--- src/components/views/Character.vue
+++ src/components/views/Character.vue
@@ -15,18 +15,48 @@
 					ul.character-info__list
 						li.character-info__list-item
 							| &nbsp;
-							.character-info__list-value
+							.character-info__list-value.modal-trigger(href='#character-info-modal')
 								| {{ aboutCharacterData[0] }}
+
+			.character__sub-content
+				.character__voice-actors.character-voice-actors
+					.character-voice-actors__title
+						| Voice Actors
+					.character-voice-actors__content
+						ul.character-voice-actors__tabs.tabs(v-if='getCharacterById.voice_actors')
+							li.character-voice-actors__tab.tab(v-for='(value, name) in getCharacterById.voice_actors'
+								:key='name')
+								a.character-voice-actors__tab-item(:href=`'#' + name`
+									v-if=`value.language === 'English' || value.language === 'Japanese'`)
+									| {{ value.name }}
+						.character-voice-actors__item(:id='name'
+							v-for='(value, name) in getCharacterById.voice_actors'
+							:key='name')
+							table.character-voice-actors__table(v-if=`value.language === 'English' || value.language === 'Japanese'`)
+								tbody.character-voice-actors__tbody
+									tr.character-voice-actors__tr
+										td.character-voice-actors__td
+											img.character-voice-actors__photo(:src='value.image_url')
+											.character-voice-actors__info
+												.character-voice-actors__lang
+													| Language: {{ value.language }}
+
+		#character-info-modal.character-modal.modal
+			.character-modal__content.modal-content
+				p.character-modal__text
+					| {{ aboutCharacterData[0] }}
 
 </template>
 
 <script>
 
 	import { mapActions, mapGetters } from 'vuex';
+	import Cards from '@/components/modules/Cards';
+	import SwiperCarousel from '@/components/modules/SwiperCarousel';
 
 	export default {
 		name: 'Character',
-		data:()=>{
+		data:() => {
 			return {
 				aboutCharacterData: []
 			}
@@ -35,6 +65,10 @@
 			return {
 				title: `Character / ${ this.getCharacterById.name }`
 			}
+		},
+		components: {
+			Cards,
+			SwiperCarousel
 		},
 		methods: {
 			...mapActions(['loadCharacterById']),
@@ -50,6 +84,11 @@
 		async created() {
 			await this.loadCharacterById();
 			await this.replaceData();
+			const modal = document.querySelectorAll('.modal');
+			const modal_instance = M.Modal.init(modal);
+			const tabs = document.querySelectorAll('.tabs');
+			const instanceTabs = M.Tabs.init(tabs);
+			const indicatorTooltip = document.querySelector('.indicator').style.display = 'none';
 		}
 	};
 
@@ -57,10 +96,15 @@
 
 <style lang='sass' scoped>
 
+	// IMPORTS
+
 	@import '../../assets/styles/utils/vars'
 	@import '../../assets/styles/utils/mixins'
+	@import '../../assets/styles/modules/titles'
 	@import '../../assets/styles/modules/dividers'
 	@import '../../assets/styles/modules/containers'
+
+	// CHARACTER
 
 	.character
 		width: 100%
@@ -68,14 +112,41 @@
 		&__container
 			display: grid
 			grid-gap: 20px
-			grid-template-columns: 1fr
+			grid-template-columns: 1fr 1fr
+			grid-template-areas: 'main sub'
 			@extend .container-default
+			+mq(tablet-mid, max)
+				grid-template-columns: 1fr
+				grid-template-areas: 'main' 'sub'
 
 		&__main-content
+			grid-area: main
 			display: grid
 			justify-content: start
 			grid-template-areas: 'title title' 'cover info'
 			grid-gap: 20px
+			align-content: start
+			grid-template-rows: 70px auto
+			+mq(phablet, max)
+				grid-template-rows: auto
+				grid-template-areas: 'title' 'cover' 'info'
+
+		&__sub-content
+			grid-area: sub
+			display: grid
+			grid-gap: 20px
+			justify-content: center
+			align-content: flex-start
+			grid-template-columns: minmax(auto, 360px)
+			+mq(tablet-mid, max)
+				justify-content: flex-start
+
+		&__cover
+			grid-area: cover
+			max-width: 220px
+
+		&__info
+			grid-area: info
 
 		&__title
 			text-align: start
@@ -83,12 +154,47 @@
 			max-width: 460px
 			grid-area: title
 
-		&__cover
-			grid-area: cover
+	// CHARACTER VOICE ACTORS
+
+	.character-voice-actors
+		&__title
+			height: 70px
+			display: flex
+			align-items: center
+			@extend .header-title
+
+		&__photo
+			width: 100%
+			max-width: 140px
+
+		&__tabs
+			display: flex
+			flex-wrap: wrap
+			overflow-x: initial
+			overflow-y: initial
+			height: 100%
+
+		&__tr
+			border-bottom: none
+
+		&__td
+			display: grid
+			grid-template-columns: 1fr 1fr
+			grid-gap: 10px
+
+		&__tab-item
+			padding: 0 !important
+			margin: 0 24px 0 0
+			text-align: start
+			color: $color-blue-light !important
+			&.active
+				background-color: initial !important
+				color: $color-orange !important
+
+	// CHARACTER INFO
 
 	.character-info
 		text-align: start
-		grid-area: info
 		+flex(initial, initial, column)
 
 		&__title
@@ -104,8 +210,9 @@
 		&__list
 			margin: 20px 0 0 5px
 			height: 100%
+			max-width: 300px
 			width: 100%
-			+flex(space-between, initial, column)
+			+flex(flex-start, initial, column)
 
 		&__list-item
 			margin: 6px 0
@@ -113,7 +220,13 @@
 				margin: 0
 
 		&__list-value
-			display: inline-block
+			white-space: pre-wrap
+			text-overflow: ellipsis
+			max-height: 300px
+			overflow: hidden
+			display: -webkit-box
+			-webkit-line-clamp: 12
+			-webkit-box-orient: vertical
 			&_decor
 				padding: 0 6px
 				border-radius: 4px
@@ -129,5 +242,12 @@
 			&:hover
 				cursor: pointer
 				border-bottom: 1px dashed $color-blue
+
+	// CHARACTER MODAL
+
+	.character-modal
+		&__text
+			white-space: pre-wrap
+			text-align: start
 
 </style>
