82c0e5b19944fe5705568101b5527d07d81cea62
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-09 22:30:15 +0300

    refactor navbar to bem

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -1,13 +1,13 @@
 <template lang='pug'>
 
-	nav.nav-global
-		.nav-wrapper.nav-global-wrapper
-			router-link.nav-global-logo.nav-global-link(to='/')
+	nav.navbar
+		.navbar__wrapper.nav-wrapper
+			router-link.navbar__logo(to='/')
 				| Otaku Library
-			.nav-global-login(v-if='this.user[0] === null')
-				router-link.nav-global-login-link(to='/login')
+			.navbar__login(v-if='this.user[0] === null')
+				router-link.navbar__login-link(to='/login')
 					| Login
-					i.material-icons.nav-global-login-icon exit_to_app
+					i.navbar__icon.material-icons exit_to_app
 
 </template>
 
@@ -37,38 +37,38 @@
 	@import '../../assets/styles/utils/vars'
 	@import '../../assets/styles/utils/mixins'
 
-	.nav
-		&-global
+	.navbar
+		height: 54px
+		line-height: 54px
+		background-color: $color-blue
+		top: 0
+		position: fixed
+		z-index: 99
+		padding: 0 40px 0 40px
+
+		&__wrapper
+			width: 100%
+			+flex(center, center, row)
+
+		&__logo
+			font-size: 24px
+			color: $color-white
+			font-family: 'Days One', sans-serif
+
+		&__login
+			position: absolute
+			right: 0
+			top: 0
+
+		&__login-link
+			+flex(center, center, row)
+			&:hover
+				.navbar__icon
+					transform: translateX(4px)
+
+		&__icon
+			margin: 0 0 0 6px
 			height: 54px
 			line-height: 54px
-			background-color: $color-blue
-			top: 0
-			position: fixed
-			z-index: 99
-			padding: 0 40px 0 40px
-
-			&-wrapper
-				width: 100%
-				+flex(center, center, row)
-
-			&-logo
-				font-size: 24px
-				color: $color-white
-				font-family: 'Days One', sans-serif
-
-			&-login
-				position: absolute
-				right: 0
-				top: 0
-
-				&-link
-					+flex(center, center, row)
-					&:hover
-						.nav-global-logout-icon
-							transform: translateX(4px)
-				&-icon
-					margin: 0 0 0 6px
-					height: 54px
-					line-height: 54px
 
 </style>
