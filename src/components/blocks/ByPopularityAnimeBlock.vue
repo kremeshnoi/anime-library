039e5e260e74ca7e7bcc4da58e74ac8183bad872
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-13 20:02:34 +0300

    create AnimeByPopularity

---
+++ src/components/blocks/ByPopularityAnimeBlock.vue
@@ -0,0 +1,65 @@
+<template lang='pug'>
+
+	.by-popularity-anime-block
+		.by-popularity-anime-block__container
+			router-link.by-popularity-anime-block__title(to='/popular-anime')
+				| Popular Anime
+				.by-popularity-anime-block__icon.material-icons keyboard_arrow_right
+			swiper-carousel
+				cards.swiper-slide(v-for='(result, index) in getAnimeByPopularity.slice(0, 7)'
+					:key='index'
+					:query='result')
+
+</template>
+
+<script>
+
+	import { mapActions, mapGetters } from 'vuex';
+	import Cards from '@/components/modules/Cards';
+	import SwiperCarousel from '@/components/modules/SwiperCarousel';
+
+	export default {
+		name: 'ByPopularityAnimeBlock',
+		components: {
+			Cards,
+			SwiperCarousel
+		},
+		methods: {
+			...mapActions(['loadAnimeByPopularity'])
+		},
+		computed: {
+			...mapGetters(['getAnimeByPopularity'])
+		},
+		created() {
+			this.loadAnimeByPopularity();
+		}
+	};
+
+</script>
+
+<style lang='sass' scoped>
+
+	@import '../../assets/styles/utils/vars'
+	@import '../../assets/styles/utils/mixins'
+	@import '../../assets/styles/modules/titles'
+
+	.by-popularity-anime-block
+		width: 100%
+
+		&__container
+			margin: 0 auto
+			padding: 40px 40px 40px 40px
+			max-width: 1300px
+
+		&__title
+			@extend .title-bordered
+			+flex(space-between, center, row)
+			&:hover
+				color: orange
+				.top-airing-block__icon
+					color: inherit
+
+		&__icon
+			color: $color-black
+
+</style>
---
+++ src/components/views/AnimeByPopularity.vue
@@ -0,0 +1,68 @@
+<template lang='pug'>
+
+	.anime-by-popularity
+		.anime-by-popularity__container
+			.anime-by-popularity__title
+				| Top Airing Anime
+
+			query-content
+				cards(v-for='(result, index) in getAnimeByPopularity'
+					:key='index'
+					:query='result')
+
+</template>
+
+<script>
+
+	import { mapActions, mapGetters } from 'vuex';
+	import Cards from '@/components/modules/Cards';
+	import QueryContent from '@/components/modules/QueryContent';
+
+	export default {
+		name: 'AnimeByPopularity',
+		components: {
+			Cards,
+			QueryContent
+		},
+		metaInfo: {
+			title: 'Otaku Library / Top raited airing anime'
+		},
+		methods: {
+			...mapActions(['loadAnimeByPopularity'])
+		},
+		computed: {
+			...mapGetters(['getAnimeByPopularity'])
+		},
+		created() {
+			this.loadAnimeByPopularity();
+		}
+	}
+
+</script>
+
+<style lang='sass' scoped>
+
+	@import '../../assets/styles/utils/vars'
+	@import '../../assets/styles/utils/mixins'
+	@import '../../assets/styles/modules/titles'
+	@import '../../assets/styles/modules/containers'
+
+
+	.anime-by-popularity
+		&__container
+			@extend .container-default
+
+		&__title
+			color: $color-black
+			width: 100%
+			font-size: 16px
+			font-weight: 700
+			text-transform: uppercase
+			margin: 0 0 20px 0
+			text-align: start
+			@extend .title-bordered
+			+mq(phablet-small, max)
+				font-size: 20px
+				max-width: 235px
+
+</style>
