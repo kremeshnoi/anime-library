bab9c66f6e7cb8227099b0e44d43a69e0a031ce6
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-17 22:47:05 +0300

    update unauthorized layout styles

--- src/components/layouts/Unauthorized.vue
+++ src/components/layouts/Unauthorized.vue
@@ -30,9 +30,18 @@
 	@import '../../assets/styles/utils/vars'
 	@import '../../assets/styles/utils/mixins'
 
+	body
+		background-color: $color-white-dark
+
+	.navbar
+		max-width: 1300px
+
 	.content-wrapper
 		padding: 54px 0 0 0
 		background-color: $color-white-pure
+		box-shadow: 0px 0px 12px 6px rgba(0,0,0,0.4)
+		max-width: 1300px
+		margin: 0 auto
 		+flex(initial, center, column)
 
 </style>
