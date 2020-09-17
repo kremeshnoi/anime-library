b44a30b49b3f4fe3b424a1c892d6768d85948a37
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-17 23:59:36 +0300

    create hero block

--- src/components/views/main/Home.vue
+++ src/components/views/main/Home.vue
@@ -1,10 +1,10 @@
 <template lang='pug'>
 
 	.home
+		hero-block(v-if='this.user[0] === null')
 		top-airing-anime-block
 		anime-block-upcoming
 		genres-block
-
 
 </template>
 
@@ -13,25 +13,36 @@
 	import TopAiringAnimeBlock from '@/components/blocks/TopAiringAnimeBlock';
 	import AnimeBlockUpcoming from "@/components/blocks/AnimeBlockUpcoming";
 	import GenresBlock from "@/components/blocks/GenresBlock";
+	import HeroBlock from "@/components/blocks/HeroBlock";
+	import { mapActions } from "vuex";
 
 	export default {
 		name: 'Home',
 		components: {
+			HeroBlock,
 			GenresBlock,
 			TopAiringAnimeBlock,
 			AnimeBlockUpcoming
 		},
 		metaInfo: {
 			title: 'Otaku Library - Organize your own anime / manga list'
+		},
+		data:()=> {
+			return {
+				user: []
+			}
+		},
+		methods: {
+			...mapActions(['getUid'])
+		},
+		async created() {
+			await this.getUid().then(result => this.user.push(result));
 		}
 	};
 
 </script>
 
 <style lang='sass' scoped>
-
-	@import '../../../assets/styles/utils/vars'
-	@import '../../../assets/styles/utils/mixins'
 
 	.home
 		width: 100%
