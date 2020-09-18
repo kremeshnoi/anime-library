55030a46fae1b976d27dbcab77969c3f7bbb9ba7
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-09-18 16:59:04 +0300

    refactor layouts, create ActionButton

--- src/components/navigation/Outdated/Sidenav.vue
+++ src/components/navigation/Outdated/Sidenav.vue
@@ -112,8 +112,8 @@
 
 <style lang='sass' scoped>
 
-	@import '../../assets/styles/utils/vars'
-	@import '../../assets/styles/utils/mixins'
+	@import '../../../assets/styles/utils/vars'
+	@import '../../../assets/styles/utils/mixins'
 
 	.side-nav
 		transition: 0.25s
