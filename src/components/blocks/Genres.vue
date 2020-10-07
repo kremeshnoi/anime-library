8dafed454d8309dd0ab949ee1d71b645cbe71da3
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-10-08 01:28:34 +0300

    refactor Genres

--- src/components/blocks/Genres.vue
+++ src/components/blocks/Genres.vue
@@ -9,7 +9,7 @@
 
 				.genres__anime
 					a.genres__item.genres__item_anime(v-for='(genre, genre_index) in result.data'
-						:key='genre_index' @click='computeRouteByGenre(genre)')
+						:key='genre_index' @click='computeRouteByGenre({result, genre})')
 						| {{ genre.title }}
 
 </template>
@@ -19,117 +19,24 @@
 	// IMPORTS
 
 	import { mapActions, mapGetters } from 'vuex';
+	import Genres from '../../utils/genres'
 
 	// COMPONENT OPTIONS
 
 	export default {
 		name: 'Genres',
-		data: () => {
-			return {
-				//Hardcode part :c
-				//Request with a list of genres will be available in v4-alpha
-				animeGenres: [
-					{ "id": 1, "title": "Action","type": "anime" },
-					{ "id": 2, "title": "Adventure","type": "anime" },
-					{ "id": 3, "title": "Cars","type": "anime" },
-					{ "id": 4, "title": "Comedy","type": "anime" },
-					{ "id": 5, "title": "Dementia","type": "anime" },
-					{ "id": 6, "title": "Demons","type": "anime" },
-					{ "id": 7, "title": "Mystery","type": "anime" },
-					{ "id": 8, "title": "Drama","type": "anime" },
-					{ "id": 9, "title": "Ecchi","type": "anime" },
-					{ "id": 10, "title": "Fantasy","type": "anime" },
-					{ "id": 11, "title": "Game","type": "anime" },
-					{ "id": 12, "title": "Hentai","type": "anime" },
-					{ "id": 13, "title": "Historical","type": "anime" },
-					{ "id": 14, "title": "Horror","type": "anime" },
-					{ "id": 15, "title": "Kids","type": "anime" },
-					{ "id": 16, "title": "Magic","type": "anime" },
-					{ "id": 17, "title": "Martial Arts","type": "anime" },
-					{ "id": 18, "title": "Mecha","type": "anime" },
-					{ "id": 19, "title": "Music","type": "anime" },
-					{ "id": 20, "title": "Parody","type": "anime" },
-					{ "id": 21, "title": "Samurai","type": "anime" },
-					{ "id": 22, "title": "Romance","type": "anime" },
-					{ "id": 23, "title": "School","type": "anime" },
-					{ "id": 24, "title": "Sci-Fi","type": "anime" },
-					{ "id": 25, "title": "Shoujo","type": "anime" },
-					{ "id": 26, "title": "Shoujo Ai","type": "anime" },
-					{ "id": 27, "title": "Shounen","type": "anime" },
-					{ "id": 28, "title": "Shounen Ai","type": "anime" },
-					{ "id": 29, "title": "Space","type": "anime" },
-					{ "id": 30, "title": "Sports","type": "anime" },
-					{ "id": 31, "title": "Super Power","type": "anime" },
-					{ "id": 32, "title": "Vampire","type": "anime" },
-					{ "id": 33, "title": "Yaoi","type": "anime" },
-					{ "id": 34, "title": "Yuri","type": "anime" },
-					{ "id": 35, "title": "Harem","type": "anime" },
-					{ "id": 36, "title": "Slice of Life","type": "anime" },
-					{ "id": 37, "title": "Supernatural","type": "anime" },
-					{ "id": 38, "title": "Military","type": "anime" },
-					{ "id": 39, "title": "Police","type": "anime" },
-					{ "id": 40, "title": "Psychological","type": "anime" },
-					{ "id": 41, "title": "Thriller","type": "anime" },
-					{ "id": 42, "title": "Seinen","type": "anime" },
-					{ "id": 43, "title": "Josei","type": "anime" }
-				],
-				mangaGenres: [
-					{ "id": 1, "title": "Action","type": "manga" },
-					{ "id": 2, "title": "Adventure","type": "manga" },
-					{ "id": 3, "title": "Cars","type": "manga" },
-					{ "id": 4, "title": "Comedy","type": "manga" },
-					{ "id": 5, "title": "Dementia","type": "manga" },
-					{ "id": 6, "title": "Demons","type": "manga" },
-					{ "id": 7, "title": "Mystery","type": "manga" },
-					{ "id": 8, "title": "Drama","type": "manga" },
-					{ "id": 9, "title": "Ecchi","type": "manga" },
-					{ "id": 10, "title": "Fantasy","type": "manga" },
-					{ "id": 11, "title": "Game","type": "manga" },
-					{ "id": 12, "title": "Hentai","type": "manga" },
-					{ "id": 13, "title": "Historical","type": "manga" },
-					{ "id": 14, "title": "Horror","type": "manga" },
-					{ "id": 15, "title": "Kids","type": "manga" },
-					{ "id": 16, "title": "Magic","type": "manga" },
-					{ "id": 17, "title": "Martial Arts","type": "manga" },
-					{ "id": 18, "title": "Mecha","type": "manga" },
-					{ "id": 19, "title": "Music","type": "manga" },
-					{ "id": 20, "title": "Parody","type": "manga" },
-					{ "id": 21, "title": "Samurai","type": "manga" },
-					{ "id": 22, "title": "Romance","type": "manga" },
-					{ "id": 23, "title": "School","type": "manga" },
-					{ "id": 24, "title": "Sci-Fi","type": "manga" },
-					{ "id": 25, "title": "Shoujo","type": "manga" },
-					{ "id": 26, "title": "Shoujo Ai","type": "manga" },
-					{ "id": 27, "title": "Shounen","type": "manga" },
-					{ "id": 28, "title": "Shounen Ai","type": "manga" },
-					{ "id": 29, "title": "Space","type": "manga" },
-					{ "id": 30, "title": "Sports","type": "manga" },
-					{ "id": 31, "title": "Super Power","type": "manga" },
-					{ "id": 32, "title": "Vampire","type": "manga" },
-					{ "id": 33, "title": "Yaoi","type": "manga" },
-					{ "id": 34, "title": "Yuri","type": "manga" },
-					{ "id": 35, "title": "Harem","type": "manga" },
-					{ "id": 36, "title": "Slice of Life","type": "manga" },
-					{ "id": 37, "title": "Supernatural","type": "manga" },
-					{ "id": 38, "title": "Military","type": "manga" },
-					{ "id": 39, "title": "Police","type": "manga" },
-					{ "id": 40, "title": "Psychological","type": "manga" },
-					{ "id": 41, "title": "Thriller","type": "manga" },
-					{ "id": 42, "title": "Seinen","type": "manga" },
-					{ "id": 43, "title": "Josei","type": "manga" }
-				]
-			}
-		},
 		computed: {
 			genres() {
 				const genres = {
 					anime_genres: {
 						title: 'Anime',
-						data: this.animeGenres
+						type: 'anime',
+						data: Genres.data
 					},
 					manga_genres: {
 						title: 'Manga',
-						data: this.mangaGenres
+						type: 'manga',
+						data: Genres.data
 					}
 				};
 
@@ -184,7 +91,7 @@
 
 			&_anime, &_manga
 				$colors: $color-blue, $color-blue-light, $color-blue-dark
-				$repeat: 20
+				$repeat: 50
 				@for $i from 1 through $repeat
 					&:nth-child(#{length($colors)}n+#{$i})
 						background: lighten(nth($colors, random(length($colors))), 20%)
