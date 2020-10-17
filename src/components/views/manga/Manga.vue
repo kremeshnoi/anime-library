662ba15450aa0a2f0091d5bb5101bd5a8efd34f8
kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-10-17 14:26:25 +0300

    move title Characters to separate component

--- src/components/views/manga/Manga.vue
+++ src/components/views/manga/Manga.vue
@@ -16,18 +16,7 @@
 				info.manga__info(:infoData='getMangaById')
 
 			.manga__sub-content
-				.manga__characters.manga-characters(v-if='getMangaCharacters.characters')
-					.manga-recommendations__wrapper
-						.manga-recommendations__title
-							| CHARACTERS
-
-						swiper-carousel(v-if='getMangaCharacters.characters' :type='`characters`')
-							cards.swiper-slide(v-for='(result, index) in getMangaCharacters.characters.slice(0, 20)'
-								:key='index'
-								:cardsData='result')
-
-						h4.manga-related__disaster(v-if='getMangaCharacters.characters.length === 0')
-							| Not found (￣︿￣)
+				characters.manga__characters(:charactersData='getMangaCharacters.characters')
 
 				related.manga__related(:relatedData='getMangaById.related')
 
@@ -42,15 +31,13 @@
 	// IMPORTS
 
 	import { mapActions, mapGetters } from 'vuex';
-	import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
 	import Cards from '@/components/elements/Cards';
-	import SwiperCarousel from '@/components/elements/SwiperCarousel';
 	import SelectOptions from '@/components/elements/SelectOptions';
 	import Related from '@/components/elements/Related';
 	import Description from '@/components/elements/Description';
 	import Recommendations from '@/components/elements/Recommendations';
-	import Info from "@/components/elements/Info";
-
+	import Info from '@/components/elements/Info';
+	import Characters from '@/components/elements/Characters';
 
 	// COMPONENT OPTIONS
 
@@ -62,18 +49,13 @@
 			}
 		},
 		components: {
+			Characters,
 			Info,
 			Recommendations,
 			Description,
 			Cards,
-			SwiperCarousel,
 			SelectOptions,
 			Related,
-			Swiper,
-			SwiperSlide
-		},
-		directives: {
-			swiper: directive
 		},
 		computed: {
 			...mapGetters(['getMangaById', 'getMangaCharacters', 'getMangaRecommendationsById'])
@@ -172,21 +154,5 @@
 		&__input-field
 			margin: 0
 			max-width: 220px
-
-	// MANGA RECOMMENDATIONS
-
-	.manga-recommendations
-		grid-area: recommendations
-
-		&__disaster
-			text-align: start
-			font-size: 30px
-
-		&__wrapper
-			display: grid
-			grid-gap: 20px
-
-		&__title
-			@extend .title-default
 
 </style>
