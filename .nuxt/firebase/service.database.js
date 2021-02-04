c251f20a02944743813b39bf903985059ad36857
kremeshnoi <alexnader.kremeshnoi@gmail.com>
2021-02-04 18:51:58 +0200

    update nuxt config

--- .nuxt/firebase/service.database.js
+++ .nuxt/firebase/service.database.js
@@ -1,7 +1,7 @@
 
 
 export default async function (session) {
-  await import('firebase/database')
+  await import(/* webpackChunkName: 'firebase-database' */'firebase/database')
 
   const databaseService = session.database()
 
