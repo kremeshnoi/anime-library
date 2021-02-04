65ad20ad5212b722b6736980dec81781bad5b3de
kremeshnoi <alexnader.kremeshnoi@gmail.com>
2021-02-04 19:04:31 +0200

    remove nuxt folder

--- .nuxt/firebase/service.database.js
+++
@@ -1,9 +0,0 @@
-
-
-export default async function (session) {
-  await import(/* webpackChunkName: 'firebase-database' */'firebase/database')
-
-  const databaseService = session.database()
-
-  return databaseService
-}
