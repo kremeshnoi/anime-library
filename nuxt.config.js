567c99b2efcd32cc9b51bedbbd81ffab7585cdb4
kremeshnoi <alexander.kremeshnoi@gmail.com>
2021-02-04 20:14:05 +0200

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }
     ]
   },
   target: 'static',
