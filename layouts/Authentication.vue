19a855aac7709331a59c111102404adeafb69d33
kremeshnoi <alexander.kremeshnoi@gmail.com>
2021-03-23 18:12:47 +0200

    fir for audit

--- layouts/Authentication.vue
+++ layouts/Authentication.vue
@@ -25,6 +25,11 @@ export default {
   },
   mounted() {
     M.AutoInit()
+    const head = document.getElementsByTagName("head")[0]
+    const script = document.createElement("script")
+    script.type = "text/javascript"
+    script.src = "https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit"
+    head.appendChild(script)
   }
 }
 </script>
